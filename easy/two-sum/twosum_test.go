cf3e46f10b2a26c1f84c895f1bc7f6f3f2d71d04
LethalWarrior <albertfiraz@gmail.com>
2023-09-19 07:24:45 +0700

    feat: add basic test cases

--- easy/two-sum/twosum_test.go
+++ easy/two-sum/twosum_test.go
@@ -1,7 +1,50 @@
-package easy_test
+package easy
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestTwoSum(t *testing.T) {
-	// Test implementation here
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "should return [0,1] when given [2,7,11,15] and 9",
+			args: args{
+				nums:   []int{2, 7, 11, 15},
+				target: 9,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "should return [1,2] when given [3,2,4] and 6",
+			args: args{
+				nums:   []int{3, 2, 4},
+				target: 6,
+			},
+			want: []int{1, 2},
+		},
+		{
+			name: "should return [0,1] when given [3,3] and 6",
+			args: args{
+				nums:   []int{3, 3},
+				target: 6,
+			},
+			want: []int{0, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TwoSum(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TwoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
