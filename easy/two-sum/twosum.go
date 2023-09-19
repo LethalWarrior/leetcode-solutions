d06ab45190f6e6ad147f2c172c70bcf26d79b779
LethalWarrior <albertfiraz@gmail.com>
2023-09-19 07:38:45 +0700

    feat: implement TwoSum()

--- easy/two-sum/twosum.go
+++ easy/two-sum/twosum.go
@@ -1,5 +1,15 @@
 package easy
 
-func TwoSum(nums[]int, target int) []int {
+func TwoSum(nums []int, target int) []int {
+	diffs := map[int]int{}
+
+	for i, num := range nums {
+		diff := target - num
+		if v, ok := diffs[diff]; ok {
+			return []int{v, i}
+		}
+		diffs[num] = i
+	}
+
 	return []int{}
 }
