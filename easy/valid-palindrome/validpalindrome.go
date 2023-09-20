fa5272f62e083caf9fa00c8e364bdcfaef48cd17
LethalWarrior <albertfiraz@gmail.com>
2023-09-20 21:55:27 +0700

    feat(valid-palindrome): implement IsPalindrome()

--- easy/valid-palindrome/validpalindrome.go
+++ easy/valid-palindrome/validpalindrome.go
@@ -1,5 +1,23 @@
 package validpalindrome
 
+import (
+	"regexp"
+	"strings"
+)
+
 func IsPalindrome(s string) bool {
+	s = strings.ToLower(s)
+	re := regexp.MustCompile(`[\s\W_]`)
+	s = re.ReplaceAllString(s, "")
+
+	j := len(s) - 1
+
+	for i := 0; i < j; i++ {
+		if s[i] != s[j] {
+			return false
+		}
+		j--
+	}
+
 	return true
 }
