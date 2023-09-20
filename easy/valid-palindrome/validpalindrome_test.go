130c72d9f6153cee6261341969441d55ddb6e99d
LethalWarrior <albertfiraz@gmail.com>
2023-09-20 08:01:41 +0700

    feat(valid-palindrome): add some test cases

--- easy/valid-palindrome/validpalindrome_test.go
+++ easy/valid-palindrome/validpalindrome_test.go
@@ -11,7 +11,21 @@ func TestIsPalindrome(t *testing.T) {
 		args args
 		want bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should return true when given 'A man, a plan, a canal: Panama'",
+			args: args{s: "A man, a plan, a canal: Panama"},
+			want: true,
+		},
+		{
+			name: "should return true when given 'race a car'",
+			args: args{s: "race a car"},
+			want: false,
+		},
+		{
+			name: "should return true when given empty string",
+			args: args{s: " "},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
