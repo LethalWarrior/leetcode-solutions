86e759c32ddc3baf3b0d01d699e779e4e4218574
LethalWarrior <albertfiraz@gmail.com>
2023-09-22 06:54:16 +0700

    feat(roman-to-integer): add some test cases

--- easy/roman-to-integer/romantointeger_test.go
+++ easy/roman-to-integer/romantointeger_test.go
@@ -11,7 +11,21 @@ func TestRomanToInt(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should return 3 when given III",
+			args: args{s: "III"},
+			want: 3,
+		},
+		{
+			name: "should return 58 when given LVIII",
+			args: args{s: "LVIII"},
+			want: 58,
+		},
+		{
+			name: "should return 1994 when given MCMXCIV",
+			args: args{s: "MCMXCIV"},
+			want: 1994,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
