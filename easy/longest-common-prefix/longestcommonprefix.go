a1fd676d9148e11c19da44d9f82e663ede23505a
LethalWarrior <albertfiraz@gmail.com>
2023-09-22 21:39:29 +0700

    feat(longest-common-prefix): implement LongestCommonPrefix()

--- easy/longest-common-prefix/longestcommonprefix.go
+++ easy/longest-common-prefix/longestcommonprefix.go
@@ -1,5 +1,23 @@
 package longestcommonprefix
 
+import (
+	"sort"
+)
+
 func LongestCommonPrefix(strs []string) string {
-	return ""
+	n := len(strs)
+
+	if n == 1 {
+		return strs[0]
+	}
+
+	sort.Strings(strs)
+
+	for i := range strs[0] {
+		if strs[0][i] != strs[n-1][i] {
+			return strs[0][:i]
+		}
+	}
+
+	return strs[0]
 }
