cb3b3f35ada242e5a44de1e6f4429b0c50b569b1
LethalWarrior <albertfiraz@gmail.com>
2023-09-23 08:57:33 +0700

    feat(remove-element): add starter files

---
+++ easy/remove-element/removeelement_test.go
@@ -0,0 +1,24 @@
+package removeelement
+
+import "testing"
+
+func TestRemoveElement(t *testing.T) {
+	type args struct {
+		nums []int
+		val  int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RemoveElement(tt.args.nums, tt.args.val); got != tt.want {
+				t.Errorf("RemoveElement() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
