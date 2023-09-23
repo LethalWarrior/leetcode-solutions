72fa89254215fd5abb0a3389461e10c4e8287581
LethalWarrior <albertfiraz@gmail.com>
2023-09-23 12:52:06 +0700

    feat(length-of-last-word): add some test cases

--- easy/length-of-last-word/lengthoflastword_test.go
+++ easy/length-of-last-word/lengthoflastword_test.go
@@ -11,7 +11,21 @@ func TestLengthOfLastWord(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: `should return 5 when given "Hello World"`,
+			args: args{s: "Hello World"},
+			want: 5,
+		},
+		{
+			name: `should return 6 when given "luffy is still joyboy"`,
+			args: args{s: "luffy is still joyboy"},
+			want: 6,
+		},
+		{
+			name: `should return 4 when given "   fly me   to   the moon  "`,
+			args: args{s: "   fly me   to   the moon  "},
+			want: 4,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
