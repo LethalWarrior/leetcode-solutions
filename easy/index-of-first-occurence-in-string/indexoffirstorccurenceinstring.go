589db8cbd3c2c5dda0dcbd5ae08a9542200468b8
LethalWarrior <albertfiraz@gmail.com>
2023-09-23 12:27:52 +0700

    feat(find-index-of-first-occurence-in-string): implement StrStr()

--- easy/index-of-first-occurence-in-string/indexoffirstorccurenceinstring.go
+++ easy/index-of-first-occurence-in-string/indexoffirstorccurenceinstring.go
@@ -1,5 +1,14 @@
 package indexoffirstoccurenceinstring
 
 func StrStr(haystack string, needle string) int {
+	m := len(haystack)
+	n := len(needle)
+
+	for i := 0; i < m-n+1; i++ {
+		if haystack[i:i+n] == needle {
+			return i
+		}
+	}
+
 	return -1
 }
