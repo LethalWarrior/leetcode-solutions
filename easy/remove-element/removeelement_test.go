12c24f0259418a57fcc8f42c265702a2d3b1b0c1
LethalWarrior <albertfiraz@gmail.com>
2023-09-23 09:14:47 +0700

    feat(remove-element): add test cases

--- easy/remove-element/removeelement_test.go
+++ easy/remove-element/removeelement_test.go
@@ -12,7 +12,16 @@ func TestRemoveElement(t *testing.T) {
 		args args
 		want int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should return 2 when given nums = [3,2,2,3], val = 3",
+			args: args{nums: []int{3, 2, 2, 3}, val: 3},
+			want: 2,
+		},
+		{
+			name: "should return 5 when given nums = [0,1,2,2,3,0,4,2], val = 2",
+			args: args{nums: []int{0, 1, 2, 2, 3, 0, 4, 2}, val: 2},
+			want: 5,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
