029f83d5dcbc4d024d048a78b8d94cbc9ac40c9e
LethalWarrior <albertfiraz@gmail.com>
2023-09-23 13:17:19 +0700

    feat(plus-one): add some test cases

--- easy/plus-one/plusone_test.go
+++ easy/plus-one/plusone_test.go
@@ -14,7 +14,21 @@ func TestPlusOne(t *testing.T) {
 		args args
 		want []int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "should return [1,2,4] when given [1,2,3]",
+			args: args{digits: []int{1, 2, 3}},
+			want: []int{1, 2, 4},
+		},
+		{
+			name: "should return [4,3,2,2] when given [4,3,2,1]",
+			args: args{digits: []int{4, 3, 2, 1}},
+			want: []int{4, 3, 2, 2},
+		},
+		{
+			name: "should return [1,0] when given [9]",
+			args: args{digits: []int{9}},
+			want: []int{1, 0},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
